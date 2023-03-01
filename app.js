7160fdec4908dc215171e47132f3ac57c3859381
bikesh <sitikhubikesh@gmail.com>
2023-03-01 16:04:58 +0545

    express application initialization

---
+++ app.js
@@ -0,0 +1,49 @@
+const config = require("config");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+const port = config.get("app.port");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({
+    status: false,
+    data: null,
+    message: err.message ? err.message : err,
+  });
+});
+
+app.listen(port, "localhost", (err) => {
+  if (err) {
+    console.log({ err });
+  } else {
+    console.log(`Server is running on port ${port}`);
+  }
+});
